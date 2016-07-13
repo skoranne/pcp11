44c6ccfa1e981e594bcf5690ffffda88bd2074bc
Sandeep Koranne <sandeep.koranne@gmail.com>
2016-07-12 19:40:32 -0700

    Adding some example code

---
+++ hw.cpp
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////////////////
+// File    : hw.cpp
+// Author  : Sandeep Koranne (C) 2016. All rights reserved.
+// Purpose : Learning Git
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <cstdlib>
+#include <iostream>
+#include <cassert>
+
+int main( int argc, char* argv[] )
+{
+  std::cout << "Hello, World!\n";
+  return( EXIT_SUCCESS );
+}
